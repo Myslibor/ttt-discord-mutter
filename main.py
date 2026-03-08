97fe588cb253ff85654f9db55394c9b890e93ccc
Myslibor <mikiuszko@gmail.com>
2026-03-08 16:39:42 +0100

    Working version

---
+++ main.py
@@ -0,0 +1,160 @@
+import discord
+from discord.ext import commands
+from flask import Flask, request
+import threading
+import os
+from pathlib import Path
+import json
+
+intents = discord.Intents.default()
+intents.message_content = True 
+intents.voice_states = True
+intents.guilds = True
+intents.members = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+def load_config():
+    if Path("config.json").exists():
+        f = open("config.json", "r", encoding="utf-8")
+        f_temp = json.load(f)
+        f.close()
+        return f_temp
+    else:
+        default = {"GUILD_ID": 0, "BOT_TOKEN" : "-"}
+        f = open("config.json", "w", encoding="utf-8")
+        json.dump(default, f)
+        f.close()
+        print("Created default config")
+        os._exit(0)
+
+def load_id_map():
+    if Path("id_map.json").exists():
+        file = open("id_map.json",'r', encoding="utf-8")
+        file_content = file.read().strip()
+        file.close()
+        file = open("id_map.json",'r', encoding="utf-8")
+        if file_content:
+            temp_f = json.load(file)
+            file.close()
+        else:
+            temp_f = {}
+        return temp_f
+    else:
+        file = open("id_map.json",'x', encoding="utf-8")
+        file.close()
+        return {}
+    
+def save_id_map(steam_dc):
+    file = open("id_map.json",'w', encoding="utf-8")
+    json.dump(steam_dc, file)
+
+steam_to_discord = load_id_map()
+config = load_config()
+GUILD_ID = config["GUILD_ID"]
+BOT_TOKEN = config["BOT_TOKEN"]
+
+print(f"GUILD_ID set to {GUILD_ID} and BOT_TOKEN set to {BOT_TOKEN}")
+
+app = Flask(__name__)   
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+@bot.event
+async def on_command_error(ctx, error):
+    print(f"Error: {error}")
+
+@bot.command()
+async def tttbot_map(ctx, steam_id: str):
+    discord_id = ctx.author.id
+    steam_to_discord[steam_id] = discord_id
+    await ctx.send(f"Mapped SteamID64 {steam_id} to Discord ID {discord_id}")
+
+@bot.command()
+async def tttbot_end(ctx):
+    await ctx.send("Shutting down")
+    save_id_map(steam_to_discord)
+    await bot.close()
+    os._exit(0)
+
+#death of a player
+@app.route("/death", methods=["POST"])
+def player_death():
+
+    steamid = request.form.get("steamid")
+    if not steamid:
+        return "Missing steamid", 400
+
+    discord_id = steam_to_discord.get(steamid)
+    if not discord_id:
+        print(f"No Discord mapping for SteamID64 {steamid}")
+        return "No mapping", 404
+
+    async def mute_player():
+        guild = bot.get_guild(GUILD_ID)
+        if not guild:
+            print("Guild not found")
+            return
+        member = guild.get_member(discord_id)
+        if member and member.voice and not member.voice.mute:
+            await member.edit(mute=True)
+            print(f"Muted {member.display_name}")
+
+    bot.loop.call_soon_threadsafe(bot.loop.create_task, mute_player())
+    return "OK"
+
+#player resurrection
+@app.route("/res", methods=["POST"])
+def player_res():
+
+    steamid = request.form.get("steamid")
+    if not steamid:
+        return "Missing steamid", 400
+
+    discord_id = steam_to_discord.get(steamid)
+
+    if not discord_id:
+        print(f"No Discord mapping for SteamID64 {steamid}")
+        return "No mapping", 404
+
+    async def unmute_player():
+        guild = bot.get_guild(GUILD_ID)
+        if not guild:
+            print("Guild not found")
+            return
+        member = guild.get_member(discord_id)
+        if member and member.voice and member.voice.mute:
+            await member.edit(mute=False)
+            print(f"Unmuted {member.display_name}")
+
+    bot.loop.call_soon_threadsafe(bot.loop.create_task, unmute_player())
+    return "OK"
+
+#new round
+@app.route("/newround", methods=["POST"])
+def handle_new_round():
+
+    async def unmute_all():
+        guild = bot.get_guild(GUILD_ID)
+        if not guild:
+            print("Guild not found")
+            return
+        for vc in guild.voice_channels:
+            for member in vc.members:
+                if member.voice and member.voice.mute:
+                    await member.edit(mute=False)
+                    print(f"Unmuted {member.display_name}")
+
+    bot.loop.call_soon_threadsafe(bot.loop.create_task,unmute_all())
+    return "OK"
+
+
+
+def run_flask():
+    app.run(host="0.0.0.0", port=5003)
+
+threading.Thread(target=run_flask).start()
+
+bot.run(BOT_TOKEN) #
---
+++ myslibor_tttaddon_mutter/lua/autorun/server/shared.lua
@@ -0,0 +1,59 @@
+if engine.ActiveGamemode() == "terrortown" then
+	AddCSLuaFile()
+	require("chttp")
+
+	print("Starting Myslibor TTT Mutter")
+
+	local DISCORD_BOT_URL = "http://127.0.0.1:5003"
+	local lastAlive = {}
+
+
+	--http post
+	local function sendToBot(req_type, data)
+		data = data or {}
+		CHTTP({
+			url = DISCORD_BOT_URL .. req_type,
+			method = "POST",
+			parameters = data,
+			success = function(code, body, headers)
+				print("Myslibor TTT Mutter: POST success to " ..DISCORD_BOT_URL .. req_type .. ", code: " .. code)
+			end,
+			failed = function(err)
+				print("Myslibor TTT Mutter: POST failed to " .. DISCORD_BOT_URL .. req_type .. ", error: " .. err)
+			end
+		})
+	end
+
+	-- player death
+	hook.Add("PlayerDeath", "Myslibor_TTT_MuteOnDeath", function(victim)
+		if IsValid(victim) and victim:IsPlayer() then
+			print("Myslibor TTT Mutter: player " .. victim:Nick() .. " " .. victim:SteamID64() .. " is dead" )
+		sendToBot("/death", {steamid = victim:SteamID64()})
+		end
+	end)
+
+	--new round 
+	hook.Add("TTTBeginRound", "Myslibor_TTT_UnmuteAll", function()
+		print("Myslibor TTT Mutter: new round")
+		sendToBot("/newround", {})
+	end)
+
+	--end round
+	hook.Add("TTTEndRound", "Myslibor_TTT_UnmuteAll2", function()
+		print("Myslibor TTT Mutter: end round")
+		sendToBot("/newround", {})
+	end)
+
+	hook.Add("Think", "TTTDetectResurrection", function()
+		for _, ply in ipairs(player.GetAll()) do
+			if not lastAlive[ply] and ply:Alive() then
+				hook.Run("TTTResurrected", ply)
+			end
+			lastAlive[ply] = ply:Alive()
+		end
+	end)
+
+	hook.Add("TTTResurrected", "TTTUnMuteRessurected", function(ply)
+		sendToBot("/res", {steamid = ply:SteamID64()})
+	end)
+end
